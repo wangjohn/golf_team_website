980cd3aa6de01472cad7aadc709ce4a8bcce6a64
wangjohn <wangjohn@mit.edu>
2013-05-17 13:12:14 -0400

    commiting all the old files.

---
+++ OldFiles/web_scripts/web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '7dee2440a6b845b7657bc0b68677bdca47c24411b73d2f9e267c30496f0055d20f7c30fc237ee9d48b4da2f11793cf30dd2f7a02c28936e10f9687ca906d8107'
---
+++ web_scripts/web/config/environment.rb
@@ -0,0 +1,8 @@
+
+# Uncomment below to put Rails into production mode
+# ENV['RAILS_ENV'] ||= 'production'
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Web::Application.initialize!
